00600a3004137e6eec9be83ddcc6d7140837b278
andresguc <Andresguc@gmail.com>
2023-11-27 23:57:21 -0500

    Add list of heroes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const router = express.Router()
-const createStrategyController = require('./controllers/createStrategy')
+const express = require('express');
+const router = express.Router();
+const createStrategyController = require('./controllers/createStrategy');
 
-// Cambia la ruta a /createStrategy
-router.post('/createStrategy', createStrategyController)
+// Cambia la ruta a /create-strategy para que coincida con la solicitud desde index.html
+router.post('/create-strategy', createStrategyController);
 
 // Exportar el enrutador
-module.exports = router
+module.exports = router;
